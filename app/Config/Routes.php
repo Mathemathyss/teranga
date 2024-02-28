37a75b60b803711f2f70c96226c94a579e51c40e
mathyss <mathyssvotte@gmail.com>
2024-02-28 13:52:40 +0100

    routes pour client article et table

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,70 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+//routes pour la page client
+$routes->get('/gestion-client', 'Client::index', ['as' => 'Gestion_Client']);
+$routes->get('/liste-client', 'Client::listeClient', ['as' => 'liste_client']);
+
+$routes->get('/ajout-client', 'Client::ajouterClientForm', ['as' => 'Ajout_Client_Form']);
+//méthode d'ajout en base de donnnée associée
+$routes->get('/ajout-client', 'Client::ajouterClient', ['as' => 'Ajout_Client']);
+
+$routes->get('/modification-client', 'Client::modifierClientForm', ['as' => 'Modification_Client_Form']);
+//méthode de modification en base de donnnée associée
+$routes->get('/modification-client', 'Client::modifierClient', ['as' => 'Modification_Client']);
+
+$routes->get('/suppression-client', 'Client::suppressionClientForm', ['as' => 'Suppression_Client_Form']);
+//méthode de suppression en base de donnnée associée
+$routes->get('/suppression-client', 'Client::suppressionClient', ['as' => 'Suppression_Client']);
+
+
+//routes pour la page article
+$routes->get('/gestion-article', 'Article::index', ['as' => 'Gestion_Article']);
+$routes->get('/liste-article', 'Article::listeArticle', ['as' => 'Liste_Article']);
+
+$routes->get('/ajout-article', 'Article::ajouterArticleForm', ['as' => 'Ajout_Article_Form']);
+//méthode d'ajout en base de donnnée associée
+$routes->get('/ajout-article', 'Article::ajouterArticle', ['as' => 'Ajout_Article']);
+
+$routes->get('/modification-article', 'Article::modifierArticleForm', ['as' => 'Modification_Article_Form']);
+//méthode de modification en base de donnnée associée
+$routes->get('/modification-article', 'Article::modifierArticle', ['as' => 'Modification_Article']);
+
+$routes->get('/suppression-article', 'Article::suppressionArticleForm', ['as' => 'Suppression_Article_Form']);
+//méthode de suppression en base de donnnée associée
+$routes->get('/suppression-article', 'Article::suppressionArticle', ['as' => 'Suppression_Article']);
+
+//routes pour la page table
+$routes->get('/gestion-table', 'Table::index', ['as' => 'Gestion_Table']);
+$routes->get('/liste-table', 'Table::listeTable', ['as' => 'Liste_Table']);
+
+$routes->get('/ajout-table', 'Table::ajouterTableForm', ['as' => 'Ajout_Table_Form']);
+//méthode d'ajout en base de donnnée associée
+$routes->get('/ajout-table', 'Table::ajouterTable', ['as' => 'Ajout_Table']);
+
+$routes->get('/modification-table', 'Table::modifierTableForm', ['as' => 'Modification_Table_Form']);
+//méthode de modification en base de donnnée associée
+$routes->get('/modification-table', 'Table::modifierTable', ['as' => 'Modification_Table']);
+
+$routes->get('/suppression-table', 'Table::suppressionTableForm', ['as' => 'Suppression_Table_Form']);
+//méthode de suppression en base de donnnée associée
+$routes->get('/suppression-table', 'Table::suppressionTable', ['as' => 'Suppression_Table']);
+
+
+//routes pour la page reservation
+
+
+
+
+
+
+
+//routes pour la page commande
+
+
+
+
+
+//page principale
 $routes->get('/', 'Home::index');
