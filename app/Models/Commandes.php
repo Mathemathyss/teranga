9c1b2dacd5055237f16571acd7da6910a2690f43
Mathemathyss <mathyssvotte@gmail.com>
2024-05-22 10:57:17 +0200

    db2

--- app/Models/Commandes.php
+++ app/Models/Commandes.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class Commandes extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'COMMANDES';
+    protected $table            = 'commandes';
     protected $primaryKey       = 'COMMANDEID';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
@@ -44,29 +44,29 @@ class Commandes extends Model
     public function rechercherCommandes($term)
     {
         // Requête de recherche avec jointure pour récupérer les commandes, les informations de réservation et les détails de commande
-        $this->select('COMMANDES.*, r.RESERVATIONID, r.DATE_HEURE, CONCAT(cl.NOM, " ", cl.PRENOM, " (", r.RESERVATIONID, ")") AS NomPrenomReservation,
+        $this->select('commandes.*, r.RESERVATIONID, r.DATE_HEURE, CONCAT(cl.NOM, " ", cl.PRENOM, " (", r.RESERVATIONID, ")") AS NomPrenomReservation,
             GROUP_CONCAT(CONCAT(a.NOM, " (", dc.QUANTITÉ, ")") SEPARATOR ", ") AS PlatsCommandes');
-        $this->join('RESERVATION r', 'COMMANDES.RESERVATIONID = r.RESERVATIONID');
+        $this->join('RESERVATION r', 'commandes.RESERVATIONID = r.RESERVATIONID');
         $this->join('CLIENTS cl', 'r.CLIENTID = cl.CLIENTID', 'left');
-        $this->join('DETAILSCOMMANDE dc', 'COMMANDES.COMMANDEID = dc.COMMANDEID', 'left');
+        $this->join('DETAILSCOMMANDE dc', 'commandes.COMMANDEID = dc.COMMANDEID', 'left');
         $this->join('ARTICLES a', 'dc.ARTICLEID = a.ARTICLEID', 'left');
-        $this->groupBy('COMMANDES.COMMANDEID');
+        $this->groupBy('commandes.COMMANDEID');
 
         // Si le terme de recherche n'est pas vide, appliquer les conditions de recherche
         if (!empty($term)) {
             // Échapper les caractères spéciaux pour éviter les injections SQL
             $term = $this->db->escapeLikeString($term);
 
-            $this->like('COMMANDES.RESERVATIONID', $term);
+            $this->like('commandes.RESERVATIONID', $term);
             $this->orLike('r.DATE_HEURE', $term);
             $this->orLike('a.NOM', $term);
             $this->orLike('cl.NOM', $term);
             $this->orLike('cl.PRENOM', $term);
-            $this->orLike('COMMANDES.STATUT', $term);
+            $this->orLike('commandes.STATUT', $term);
         } else {
             // Si aucun terme de recherche n'est spécifié, récupérer uniquement les Commandes en cours
-            $this->where('COMMANDES.STATUT', 'Enregistrée');
-            $this->orWhere('COMMANDES.STATUT', 'En cours de préparation');
+            $this->where('commandes.STATUT', 'Enregistrée');
+            $this->orWhere('commandes.STATUT', 'En cours de préparation');
         }
 
         // Exécuter la requête
