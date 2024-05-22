8ed86843bca65398357446c0956ef9d8d97ff8e3
Mathemathyss <mathyssvotte@gmail.com>
2024-05-22 10:41:16 +0200

    aaa

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -7,7 +7,7 @@ helper('html');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php echo link_tag('inc/style.css'); ?>
+    <?php echo link_tag('public/inc/style.css'); ?>
     <title>African Teranga</title>
 </head>
 <header>
