6bf732da7b8ce06bde9ae5cda7b0d421d43d3bfe
Mathemathyss <mathyssvotte@gmail.com>
2024-05-30 15:41:39 +0200

    ui

--- app/Views/Commande/gestion-commande.php
+++ app/Views/Commande/gestion-commande.php
@@ -40,13 +40,12 @@ $term = isset($termReserv) ? $_GET['search'] : '';
         // Afficher les données de chaque commande
         foreach ($resultats as $commande) {
             $heure = $commande['DATE_HEURE'];
-            $heureForm = $heure->format('Y-m-d H:i:s');
-            $heurelivraison = ($commande['LIEU'] == 'A emporter') ? [date("Y-m-d H:i:s", strtotime('+2 hours', strtotime($heure)))] : '' ;
+            $heurelivraison = strlen(($commande['LIEU'] == 'A emporter') ? [date("Y-m-d H:i:s", strtotime('+2 hours', strtotime($heure)))] : '') ;
             //var_dump($heurelivraison);
             echo "<tr>
                     <td>{$commande['COMMANDEID']}</td>
                     <td>{$commande['NomPrenomReservation']}</td>
-                    <td>$heureForm + ($heurelivraison)</td>
+                    <td>$heure + ($heurelivraison)</td>
                     <td>{$commande['PlatsCommandes']}</td>
                     <td>{$commande['LIEU']}</td>
                     <td>{$commande['STATUT']}</td>
