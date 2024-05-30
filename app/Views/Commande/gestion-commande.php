709906a4b57242466959089e1e74db4fc5a6723f
Mathemathyss <mathyssvotte@gmail.com>
2024-05-30 15:28:47 +0200

    gogogo

--- app/Views/Commande/gestion-commande.php
+++ app/Views/Commande/gestion-commande.php
@@ -40,10 +40,11 @@ $term = isset($termReserv) ? $_GET['search'] : '';
         // Afficher les données de chaque commande
         foreach ($resultats as $commande) {
             $heurelivraison = ($commande['LIEU'] == 'A emporter') ? [date("Y-m-d H:i:s", strtotime('+2 hours', (int)$commande['DATE_HEURE']))] : '' ;
+            echo $heurelivraison;
             echo "<tr>
                     <td>{$commande['COMMANDEID']}</td>
                     <td>{$commande['NomPrenomReservation']}</td>
-                    <td>{$commande['DATE_HEURE']} +($heurelivraison)</td>
+                    <td>{$commande['DATE_HEURE']} + ()</td>
                     <td>{$commande['PlatsCommandes']}</td>
                     <td>{$commande['LIEU']}</td>
                     <td>{$commande['STATUT']}</td>
