6560282e01ea970fa00a5ba79060726acb253f5e
Mathemathyss <mathyssvotte@gmail.com>
2024-05-30 15:08:16 +0200

    test 1

--- app/Models/Commandes.php
+++ app/Models/Commandes.php
@@ -13,7 +13,7 @@ class Commandes extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['RESERVATIONID', 'DATE_HEURE', 'STATUT'];
+    protected $allowedFields    = ['RESERVATIONID', 'DATE_HEURE', 'LIEU', 'STATUT'];
 
     // Dates
     protected $useTimestamps = false;
